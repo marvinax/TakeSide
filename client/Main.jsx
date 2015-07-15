054e4760fc032772480af2798f97fbdc41c1fde4
marvinax <nightvista@gmail.com>
2015-07-15 17:22:58 +0800

    about to check

--- client/Main.jsx
+++ client/Main.jsx
@@ -18,6 +18,14 @@ var App = React.createClass({
 		this.xhr.send({url : "http://everstream.cn"});
  		this.xhr.onload = function(e){
  			console.log(this.response);
+			// wx.config({
+			// 	debug: false,
+			// 	appId: '',
+			// 	timestamp: ,
+			// 	nonceStr: '', // 必填，生成签名的随机串
+			// 	signature: '',// 必填，签名，见附录1
+			// 	jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+			// });
  		}
 	},
 
