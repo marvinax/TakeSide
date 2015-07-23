0f96ff3d69bb785f97be913d5dd475a5e49239d1
marvinax <nightvista@gmail.com>
2015-07-23 11:37:55 +0800

    changed token

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = function() {
     return {
         appId: 'wxf0db58e603fe17da',
         appSecret: 'c310111e9737e09a71588b176d92ff89',
-        appToken: 'SHANG',
+        appToken: 'tianwanggaidihu12345',
         cache_json_file: path.join(__dirname + '/cache')
     };
 };
