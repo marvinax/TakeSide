7bd7205265e068ebe42bf37f068fcca35682adba
marvinax <nightvista@gmail.com>
2015-07-25 20:12:04 +0800

    still tyring

--- client/Main.jsx
+++ client/Main.jsx
@@ -35,6 +35,7 @@ var App = React.createClass({
 		alert(JSON.stringify({url: location.href.split('#')[0]}));
 
 		this.xhr.open("POST", "/wechat", true);
+		this.xhr.setRequestHeader("Content-type", "application/json");
 		this.xhr.send(JSON.stringify({"url": location.href.split('#')[0]}));
  		this.xhr.onload = function(e){
 
